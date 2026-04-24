0b9e3b8ad9b6265e9b81422103217b8866cdafea
Hector Bonilla <hector.bonilla@ug.uchile.cl>
2026-04-24 15:40:03 -0400

    feat: la nacida persigue el mouse

--- scenes/characters/nacida.gd
+++ scenes/characters/nacida.gd
@@ -23,4 +23,17 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+
+	var mouse_vec: Vector2 = get_global_mouse_position()
+	var pos: Vector2 = global_position
+	var direction_metal = mouse_vec - pos
+	
+	# Mouse in viewport coordinates.
+	if Input.is_action_pressed("nacida_pull"):
+		velocity = direction_metal.normalized() * 500
+		print("MBL Click/Unclick at: ", velocity)
+	elif Input.is_action_pressed("nacida_push"):
+		velocity = direction_metal.normalized() * -500
+		print("MBR Click/Unclick at: ", velocity)
+
 	move_and_slide()
