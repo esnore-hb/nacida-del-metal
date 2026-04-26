403a27302a1e16e28c90d3112065b98c3779a7a3
Hector Bonilla <hector.bonilla@ug.uchile.cl>
2026-04-26 19:33:35 -0400

    feat: tirar y empujar de metales livianos

--- scenes/metals/metal_liviano.gd
+++ scenes/metals/metal_liviano.gd
@@ -1,29 +1,57 @@
 extends Node2D
 
-@onready var anclaje: Area2D = $Anclaje
+@onready var anclaje: Area2D = $Metal/Anclaje
 @onready var metal: RigidBody2D = $Metal
 
-signal pulling(direction, nacida_pos)
-signal pushing(direction, nacida_pos)
+var is_pulling: bool = false
+var is_pushing: bool = false
+var is_mouse = false
+var direc_nacida
+var posit_nacida
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pulling.connect(_pull_metal)
-	pushing.connect(_push_metal)
+	anclaje.mouse_entered.connect(_on_mouse_enter)
+	anclaje.mouse_exited.connect(_on_mouse_exit)
+	
+	if Game.nacida:
+		Game.nacida.pulling.connect(_pull_metal)
+		Game.nacida.pushing.connect(_push_metal)
+	else:
+		Game.nacida_set.connect(_nacida_generada)
 
-func _pull_metal(direction: Vector2, pos: Vector2) -> void:
-	if (Input.is_action_pressed("nacida_pull") and anclaje.mouse_entered):
-		metal.apply_impulse(direction * 500)
-		print("me estan tirando")
+func _nacida_generada():
+	Game.nacida.pulling.connect(_pull_metal)
+	Game.nacida.pushing.connect(_push_metal)
 
-func _push_metal(direction: Vector2, pos: Vector2) -> void:
-	if (Input.is_action_pressed("nacida_push") and anclaje.mouse_entered):
-		metal.apply_impulse(direction * -500)
-		print("me estan empujando")
+func _pull_metal(direction_nacida: Vector2, pos_nacida: Vector2) -> void:
+	is_pulling = true
+	direc_nacida = direction_nacida
+	posit_nacida = pos_nacida
+
+func _push_metal(direction_nacida: Vector2, pos_nacida: Vector2) -> void:
+	is_pushing = true
+	direc_nacida = direction_nacida
+	posit_nacida = pos_nacida
+
+func _on_mouse_enter() -> void:
+	is_mouse = true
+
+func _on_mouse_exit() -> void:
+	is_mouse = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (anclaje.mouse_entered):
-		print("entro")
-	elif (anclaje.mouse_exited):
-		print("salio")
+	if is_pulling and is_mouse:
+		metal.apply_impulse(-direc_nacida * 500 * delta)
+		metal.gravity_scale = 0
+		Debug.log("tirando")
+	elif is_pushing and is_mouse:
+		metal.apply_impulse( direc_nacida * 500 * delta)
+		metal.gravity_scale = 0
+		Debug.log("empujando")
+	else:
+		metal.gravity_scale = 1
+		is_pulling = false
+		is_pushing = false
