636766cb314646862784239410c6eeac0b13a9e9
Racheel1214 <rakhel15d@gmail.com>
2026-05-05 14:24:04 -0400

    feat: cambio en menus para tener level manager

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -10,10 +10,10 @@ func _ready() -> void:
 	quit.pressed.connect(_on_quit_pressed)
 	
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+	LevelManager.start()
 	
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_credits_pressed() -> void:
-	Debug.log("creditos futuros")
+	LevelManager.credits()
--- ui/pause_menu/pause_menu.gd
+++ ui/pause_menu/pause_menu.gd
@@ -3,11 +3,13 @@ extends Control
 @onready var resume: Button = %Resume
 @onready var retry: Button = %Retry
 @onready var main_menu: Button = %MainMenu
+#@onready var quit: Button = %Quit
 
 func _ready() -> void:
 	hide()
 	resume.pressed.connect(_on_resume_pressed)
 	retry.pressed.connect(_on_retry_pressed)
+	#quit.pressed.connect(_on_quit_pressed)
 	main_menu.pressed.connect(_on_main_menu_pressed)
 	
 func _input(event: InputEvent) -> void:
@@ -15,11 +17,17 @@ func _input(event: InputEvent) -> void:
 		get_tree().paused = not get_tree().paused
 		visible = get_tree().paused
 
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
+	LevelManager.main_menu()
+
 func _on_resume_pressed() -> void:
-	Debug.log("resume pressed")
+	get_tree().paused = false
+	hide()
 	
 func _on_retry_pressed() -> void:
-	Debug.log("retry pressed")
+	get_tree().paused = false
+	get_tree().reload_current_scene()
 
-func _on_main_menu_pressed() -> void:
-	Debug.log("main_manu pressed")
+func _on_quit_pressed() -> void:
+	get_tree().quit()
