e27d8b830e30e99d998efba0d77e1f6e04dae4c9
Racheel1214 <rakhel15d@gmail.com>
2026-06-07 15:00:03 -0400

    fix: goal now detect groups of pips in the levels

--- scenes/pips/pip.gd
+++ scenes/pips/pip.gd
@@ -2,13 +2,14 @@ extends Area2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animation_tree: AnimationTree = $AnimationTree
-@onready var sprite_2d: Sprite2D = $Sprite2D
+#@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _ready() -> void:
+	add_to_group("pips")
 	animation_tree.active = true
 	var state_machine = animation_tree.get("parameters/playback")
 	state_machine.travel("idle")
-	Game.pips += 1
+	#Game.pips += 1
 	body_entered.connect(_on_body_entered)
 	
 func _on_body_entered(body: Node2D) -> void:
@@ -16,7 +17,7 @@ func _on_body_entered(body: Node2D) -> void:
 	if player:
 		set_deferred("monitoring", false)
 		#pick_up_sound.play()
-		Game.pips -= 1
+		Game.pips += 1
 		var state_machine = animation_tree.get("parameters/playback")
 		state_machine.travel("pick_up")
 		await get_tree().create_timer(0.20).timeout
