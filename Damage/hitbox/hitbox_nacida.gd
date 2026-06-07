d0a276f916c834f5a58c6b63a7ac3d5faa751125
Beatriz Toledo <beatriz.toledo@ug.uchile.cl>
2026-06-07 15:13:44 -0400

    fead: se elimino funcion que no debia estar ahi

--- Damage/hitbox/hitbox_nacida.gd
+++ Damage/hitbox/hitbox_nacida.gd
@@ -4,7 +4,3 @@ extends Area2D
 # Se puede cambiar el daño en el inspector
 @export var damage: int = 10
 @export var speed: int = 600
-
-func _physics_process(delta: float) -> void:
-	var direction = global_transform.x
-	global_position += direction * speed * delta
