88b9671e72c28791460e30dd2edf7779cba636fe
Hector Bonilla <hector.bonilla@ug.uchile.cl>
2026-06-20 17:50:24 -0400

    feat: lord se mueve con nacida

--- scenes/characters/lord.gd
+++ scenes/characters/lord.gd
@@ -1,6 +1,8 @@
 class_name Lord
 extends RigidBody2D
 
+@export var MOVE_SPEED: float = 150.0
+@export var MAX_SPEED: float = 200.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,3 +12,19 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func _physics_process(_delta: float) -> void:
+	var direction: float = Input.get_axis("nacida_left", "nacida_right")
+	var force: Vector2 = Vector2.ZERO
+
+	if direction:
+		force.x = MOVE_SPEED * direction
+
+		if abs(linear_velocity.x) > MAX_SPEED:
+			linear_velocity.x = MAX_SPEED * direction
+
+	apply_central_impulse(force)
+
+
+func _integrate_forces(_state: PhysicsDirectBodyState2D) -> void:
+	rotation_degrees = 0.0
