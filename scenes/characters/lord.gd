88ca9a01d787b05c67795fe444b425d9d4416f7b
Hector Bonilla <hector.bonilla@ug.uchile.cl>
2026-07-06 11:49:01 -0400

    feat: el jefe hace dano a la nacida, con patron de ataque

--- scenes/characters/lord.gd
+++ scenes/characters/lord.gd
@@ -6,9 +6,13 @@ extends RigidBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var hitbox_lord: Area2D = $HitboxLord
+@onready var bullet_spawn_mark: Marker2D = $Marker2D
+
+@export var bullet_scene: PackedScene
 
 var nacida: Nacida
 var health: int = 3
+@onready var timer: Timer = $Timer
 
 func _ready() -> void:
 	animation_tree.active = true
@@ -18,12 +22,15 @@ func _ready() -> void:
 		nacida = Game.nacida
 	else:
 		Game.nacida_set.connect(_nacida_generada)
+	timer.start(2)
+
 
 
 func _process(_delta: float) -> void:
 	_recive_damage()
-	print(global_position)
-
+	if timer.time_left < 1:
+		fire()
+		timer.start(2)
 
 # Funcion fallback que se asegura que la nacida esta en el nivel
 func _nacida_generada():
@@ -49,3 +56,23 @@ func _recive_damage():
 		health -= 1
 		print("lord recive damage")
 		print("health: ", health)
+
+func attack():
+	pass
+	
+func fire() -> void:
+	if not bullet_scene:
+		Debug.log("ERROR: Me olvide poner la bala en el inspector")
+		return
+		
+	var bullet_inst = bullet_scene.instantiate()
+	
+	get_parent().add_child(bullet_inst)
+	
+	if not bullet_spawn_mark:
+		return
+		
+	bullet_inst.global_position = bullet_spawn_mark.global_position
+	
+	var mouse_direction = bullet_spawn_mark.global_position.direction_to(nacida.global_position)
+	bullet_inst.global_rotation = mouse_direction.angle()
