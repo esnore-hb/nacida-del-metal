75907c1940595f6b0a13216e66de82a78aaf0810
Hector Bonilla <hector.bonilla@ug.uchile.cl>
2026-07-05 23:01:57 -0400

    feat: implementacion de jefe

--- scenes/characters/lord.gd
+++ scenes/characters/lord.gd
@@ -5,29 +5,39 @@ extends RigidBody2D
 @export var MAX_SPEED: float = 200.0
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animation_tree: AnimationTree = $AnimationTree
+@onready var hitbox_lord: Area2D = $HitboxLord
 
+signal lord_defeat
 
-# Called when the node enters the scene tree for the first time.
+var nacida: Nacida
+var health: int = 3
+
 func _ready() -> void:
-	pass # Replace with function body.
+	if Game.nacida:
+		nacida = Game.nacida
+	else:
+		Game.nacida_set.connect(_nacida_generada)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	_recive_damage()
 
-func _physics_process(_delta: float) -> void:
-	var direction: float = Input.get_axis("nacida_left", "nacida_right")
-	var force: Vector2 = Vector2.ZERO
 
-	if direction:
-		force.x = MOVE_SPEED * direction
-
-		if abs(linear_velocity.x) > MAX_SPEED:
-			linear_velocity.x = MAX_SPEED * direction
-
-	apply_central_impulse(force)
+# Funcion fallback que se asegura que la nacida esta en el nivel
+func _nacida_generada():
+	nacida = Game.nacida
 
 
 func _integrate_forces(_state: PhysicsDirectBodyState2D) -> void:
 	rotation_degrees = 0.0
+
+
+func _recive_damage():
+	if hitbox_lord.get_overlapping_areas():
+		hitbox_lord.get_overlapping_areas()[0].get_parent().get_parent().queue_free()
+		if health < 0:
+			lord_defeat.emit()
+			return
+		health -= 1
+		print("lord recive damage")
+		print("health: ", health)
